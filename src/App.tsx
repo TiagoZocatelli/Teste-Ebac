b003cb7d7281e375b64f33d45a96baab0091ea41
Tiago Zocatelli <zokahtv@gmail.com>
2024-09-26 00:05:38 -0300

    Aplicando Testes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className={styles.app}>
       <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
+        Olha que legal minha miniatura do Batmóvel.
       </Post>
     </div>
   );
